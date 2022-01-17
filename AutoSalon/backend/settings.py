72a16bee3f47d5f12d01a4469b333f4873aed7c7
IlyaZakharovN <95462920+IlyaZakharovN@users.noreply.github.com>
2022-01-17 10:17:06 +0300

    Added custom user model, DRF user page.

--- AutoSalon/backend/settings.py
+++ AutoSalon/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,10 @@ INSTALLED_APPS = [
 
     'rest_framework',
     # 'rest_framework_simplejwt', # add later
+
+    'employees',
+    'cars'
+
 ]
 
 MIDDLEWARE = [
@@ -129,6 +134,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# media roots:
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# custom user model:
+AUTH_USER_MODEL = 'employees.UserAccount'
 
 
 # django rest framework + simple jwt:
