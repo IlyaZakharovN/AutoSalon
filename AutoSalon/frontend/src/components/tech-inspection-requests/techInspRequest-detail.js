c4b4809bbbcf81f07059194a1cfa7e94594e0f74
IlyaZakharovN <95462920+IlyaZakharovN@users.noreply.github.com>
2022-03-24 09:12:37 +0300

    Done tech inspection request detail component

---
+++ AutoSalon/frontend/src/components/tech-inspection-requests/techInspRequest-detail.js
@@ -0,0 +1,70 @@
+import React, { Fragment } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const TechInpectionRequestDetail = ({
+    techInspRequest, empl, car,
+    carModels, techInspection
+}) => {
+    return (
+        <Fragment>
+            <h4 className="mb-3" style={{textAlign: "center"}}>
+                {`Заявка на тех. осмотр №${techInspRequest.id} от ${new Date(techInspRequest.date).toLocaleDateString()}`}
+            </h4>
+            
+            <div style={{textAlign: "left"}} className="info">
+                {Array.isArray(car) && car.map(car => (
+                    Array.isArray(carModels) && carModels
+                    .filter(carModel => carModel.id === car.model_id)
+                    .map(carModel => (
+                        <Fragment>
+                            {techInspection.length ? (
+                                <p>Тех. остмотр
+                                    <span>{` - `}
+                                        <Link to={`/tech-inspections/${techInspection.id}`}>
+                                            {techInspection.end_date ? (
+                                                `закончен ${techInspection.end_date.toLocaleDateString()}`
+                                            ) : (
+                                                `начат ${techInspection.start_date.toLocaleDateString()}`
+                                            )}
+                                        </Link>
+                                    </span>
+                                </p>
+                            ) : (
+                                <p className="attention">
+                                    Тех. осмотр по данной заявке еще не был начат.
+                                </p>
+                            )}
+                            <p>Автомобиль 
+                                <span>{` - `}
+                                    <Link to={`/car/${car.VIN}`}>
+                                        {`${car.VIN}, ${carModel.brand} 
+                                        ${carModel.model} ${carModel.year}
+                                         в комплектации ${carModel.package_name}`}
+                                    </Link>
+                                </span>
+                                <img src={carModel.main_photo} className="mb-3"/>
+                            </p>
+                            <p>Заявка оставлена
+                                <span>
+                                    {Array.isArray(empl) && empl
+                                        .map(empl => (
+                                            `- ${empl.name}, ${empl.email}`
+                                        ))
+                                    }
+                                </span>
+                            </p>
+                            <p>Причина регистрации заявки
+                                <span>
+                                    {` - ${techInspRequest.reason}`}
+                                </span>
+                            </p>
+                        </Fragment>
+                    ))
+                ))}
+            </div>
+
+        </Fragment>
+    );
+};
--- AutoSalon/frontend/src/pages/techInspectionsRequests/TechInpectionRequest.js
+++ AutoSalon/frontend/src/pages/techInspectionsRequests/TechInpectionRequest.js
@@ -1,16 +1,106 @@
-// Try to replace return {...action.payload} to return [...action.payload]
-// in slice
-
 import React, { Fragment, useState, useEffect, useCallback } from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { TechInpectionRequestDetail } from "../../components/tech-inspection-requests/techInspRequest-detail";
+
+import { 
+    retriveCars, carsSelector 
+} from "../../slices/carSlice";
+import { 
+    getAllCarModels, carModelsSelector } from "../../slices/carModelsSlice";
+import { 
+    fetchEmplData, retriveEmplData, employeeSelector 
+} from "../../slices/employeeSlice";
+import { 
+    getAllTechInspections, techInspectionSelector
+} from "../../slices/techInspectionSlice";
+import { 
+    getAllTechInspectionRequests, 
+    fetchTechInspectionRequest,
+    techInspectionRequestSelector 
+} from "../../slices/techInspectionRequestSlice";
+import { 
+    userSelector, getUserDetails 
+} from "../../slices/userSlice";
+
 const TechInpectionRequest = () => {
+    const dispatch = useDispatch();
+    const params = useParams();
+
+    const { user, isAuthenticated } = useSelector(userSelector);
+    const cars = useSelector(carsSelector);
+    const carModels = useSelector(carModelsSelector);
+    const empls = useSelector(employeeSelector);
+    const techInspections = useSelector(techInspectionSelector);
+    const techInspectionRequests = useSelector(techInspectionRequestSelector);
+
+    const initFetch = useCallback(async() => {
+        await dispatch(getAllCarModels());
+        await dispatch(retriveCars());
+        await dispatch(getAllTechInspections());
+        // await dispatch(getAllTechInspectionRequests());
+        await dispatch(fetchTechInspectionRequest(params.id));
+        await dispatch(retriveEmplData());
+    }, [dispatch]);
+
+    useEffect(() => {
+        initFetch();
+    }, [initFetch]);
+
+    const renderDetail = () => {
+        if (
+            techInspectionRequests && empls && cars &&
+            carModels && techInspections
+        ) {
+            // const theTIR = Array.isArray(techInspectionRequests) &&
+            // techInspectionRequests
+            //     .filter(tir => tir.id == params.id)[0];
+
+            return <TechInpectionRequestDetail
+                techInspRequest={techInspectionRequests}
+                // empl={
+                //     Array.isArray(empls) && empls
+                //         .filter(empl => empl.id === theTIR.requester)
+                // }
+                empl={
+                    Array.isArray(empls) && empls
+                        .filter(empl => empl.id === techInspectionRequests.requester)
+                }
+                car={
+                    Array.isArray(cars) && cars
+                        .filter(car => car.VIN === techInspectionRequests.VIN)
+                }
+                carModels={carModels}
+                techInspection={
+                    Array.isArray(techInspections) && techInspections
+                        .filter(tI => tI.request === techInspectionRequests.id)
+                }
+            />
+        } else {
+            return <p>Ожидание загрузки информации о заявке...</p>
+        }
+    };
     
     return (
-        <section></section>
+        isAuthenticated ? (
+            (!techInspectionRequests && !empls && !cars &&
+            !carModels && !techInspections) ? (
+                <div>Ожидание загрузки данных</div>
+            ) : (
+                <section>
+                    <Row className="mt-3 justify-content-md-center">
+                        <Col xs lg="6">
+                            {renderDetail()}
+                        </Col>
+                    </Row>
+                </section>
+            )
+        ) : (
+            <div>Страница не найдена</div>
+        )
     );
 };
 
